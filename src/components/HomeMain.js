53f9ed838802e39cb13828ac32526f9df3fddbc1
yogesh16singh <ys1997642@gmail.com>
2023-04-05 17:25:29 +0530

    main top completed

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -6,16 +6,16 @@ const HomeMain = () => {
             <main className='home_main'>
                 <div className='main_top' >
                     <section className='section_left'>
-                        <h1> Front-End React <br></br>Developer</h1>
-                        <p className='section_left_p'>Hi, I'm Yogesh Singh. A passionate Front-end React Developer based in Beawar , Rajasthan .</p>
+                        <h1> Front-End React <br></br>Developer  <h1>✔</h1></h1>
+                        <p className='section_left_p'>Hi, I'm Yogesh Singh😎. A passionate Front-end React Developer based in Beawar , Rajasthan 🏡.</p>
                         <div className='section_left_div'>
-                            <a href='#'> <i class="fa-brands fa-linkedin section_left_i"></i></a>
-                            <a href='#'>  <i class="fa-brands fa-github section_left_i"></i></a>
+                            <a href='#linkedln'> <i class="fa-brands fa-linkedin section_left_i"></i></a>
+                            <a href='#github'>  <i class="fa-brands fa-github section_left_i"></i></a>
                         </div>
                     </section>
                     <section className='section_right'>
                         <div>
-                            <img src={bg} width={400}></img>
+                            <img src={bg} width={400} alt='yogesh'></img>
                         </div>
                     </section>
 
